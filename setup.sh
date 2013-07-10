6364fb561d1b2ae91b9de2af440c537e9dfe98dc
Luke Liu <lukekliuster@gmail.com>
2013-07-10 21:48:09 +0000

    Added git config

--- setup.sh
+++ setup.sh
@@ -2,9 +2,17 @@
 # Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
 # for headless setup. 
 
+# Install git
+sudo apt-get install -y git-core
+git config --global user.name "Luke Liu"
+git config --global user.email lukekliuster@gmail.com
+
+
+# Install heroku
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
 # Install nvm: node-version manager
 # https://github.com/creationix/nvm
-sudo apt-get install -y git-core
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
 # Load nvm and install latest production node
@@ -15,6 +23,8 @@ nvm use v0.10.12
 # Install jshint to allow checking of JS code within emacs
 # http://jshint.com/
 npm install -g jshint
+npm install cheerio
+npm install commander
 
 # Install rlwrap to provide libreadline features with node
 # See: http://nodejs.org/api/repl.html#repl_repl
